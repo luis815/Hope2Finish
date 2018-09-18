fe63230fc37b5f7945ffc0779d2a8a9563ac10ec
Luis Lopez Cardona <lalopezc@protonmail.com>
2018-09-18 15:49:23 -0400

    Proper vue setup
    Sorry about the issue :(

---
+++ front_end/src/router.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import SampleHome from './views/SampleHome.vue'
+import LandingPage from './views/LandingPage.vue'
+import Profile from './views/Profile.vue'
+import LogIn from './views/LogIn.vue'
+import SignUp from './views/SignUp.vue'
+import Home from './views/Home.vue'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'landing-page',
+            component: LandingPage
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: SignUp
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LogIn
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: Profile
+        },
+        {
+            path: '/sample-home',
+            name: 'sample-home',
+            component: SampleHome
+        },
+        {
+            path: '/sample-about',
+            name: 'sample-about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/SampleAbout.vue')
+        }
+    ]
+})
---
+++ front_end/src/views/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="LandingPage">
+        <h1>Hello World!</h1>
+        <p>This is our cse442 project.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LandingPage"
+    }
+</script>
+
+<style scoped>
+    #LandingPage {
+
+    }
+</style>
