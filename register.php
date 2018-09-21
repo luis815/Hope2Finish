12140a7897378157665435914f8a58c8e8fa07c2
Michael Belker <mbelker@teamofweaponsandtactics.com>
2018-09-21 01:42:49 -0400

    Finished the registration form (with exception of storing data anywhere). Finished functions for testing submission data, added stub function for sanitizing output, remove unused variables, add error messages for each issue and also message for success. Added current course and session for proof that it is ours during demos.

--- register.php
+++ register.php
@@ -54,11 +54,21 @@ button {
     font-style: normal;
 }
 
+#error {
+	margin: 0 auto;
+	margin-top: 10px;
+	width: 60%;
+	min-width: 200px;
+	color: rgb(255, 0, 0);
+}
+
 
 ';
 
-$html_begin = '<html><head><title>Register</title>' . $external_css . '<style> ' . $css . '</style></head><body>';
+$html_begin = '<html><head><title>Register | FALL_2018_CSE442</title>' . $external_css . '<style> ' . $css . '</style></head><body>';
 $html_end = '</body></html>';
+$html_error_begin = '<div id="error">';
+$html_error_end = '</div>';
 
 
 $form_signup = 
@@ -99,7 +109,6 @@ function CONTAINS($data, $search){
 	return false;
 }
 
-
 //	@summary	Checks if a form was submitted with any data. Does not check validity of input.
 //	@return		Returns true if form was submitted with any data.
 function WAS_FORM_SUBMITTED(){
@@ -115,10 +124,10 @@ function WAS_FORM_SUBMITTED(){
 
 //	@summary	Checks if submitted form data is valid.
 //	@return		Returns true if form was submitted with valid data.
-//	@$error		This return var indicates reason for invalid data (0 = missing/blank fields; 1 = invalid email; ... )
+//	@$error		This return var indicates reason for invalid data:
+//				0 = missing/blank fields; 1 = invalid email; 2 = username too short/long;
+//				3 = password's do not match; 4 = password too short/long;
 function IS_FORM_DATA_VALID(&$error){
-	
-	$result = false;
 	
 	//	Ensure fields are set and not blank.
 	if (!isset($_REQUEST['email']) || empty($_REQUEST['email']) ||
@@ -139,16 +148,36 @@ function IS_FORM_DATA_VALID(&$error){
 	}
 	
 	//	Does username meet minimum length requirements?
-	
-	
-	
-	//	Does password meet minimum length requirements?
-	
+	if ((strlen($_REQUEST['username']) < MIN_USERNAME_LENGTH) || (strlen($_REQUEST['username']) > MAX_USERNAME_LENGTH)){
+		
+		$error = 2;
+		return false;
+	}
 	
 	//	Do both password fields match?
+	if (strcmp($_REQUEST['password1'], $_REQUEST['password2']) != 0){
+		
+		$error = 3;
+		return false;
+	}
 	
+	//	Does password meet minimum length requirements?
+	if ((strlen($_REQUEST['password1']) < MIN_PASSWORD_LENGTH) || (strlen($_REQUEST['password1']) > MAX_PASSWORD_LENGTH)){
+		
+		$error = 4;
+		return false;
+	}
 	
+	return true;
+}
+
+
+//	@summary	Sanitizes submitted form data. The sanitized email, username, and password
+//				are returned in the variables passed by reference.
+//	@return		void
+function GET_FORM_DATA(&$email, &$username, &$password){
 	
+	//TODO
 }
 
 
@@ -160,14 +189,59 @@ function IS_FORM_DATA_VALID(&$error){
 //	-------------------------------------------------------------------------------------
 
 //	Check if all fields have been submitted.
+if (WAS_FORM_SUBMITTED()){
+	
+	$error = 0;
+	if (!IS_FORM_DATA_VALID($error)){
+		
+		switch($error){
+			case 0:
+				$_SESSION['error_message'] = "Please fill out <u>all</u> fields.";
+				break;
+			case 1:
+				$_SESSION['error_message'] = "The <u>email address</u> you provided appears to be invalid.";
+				break;
+			case 2:
+				$_SESSION['error_message'] = "Your <u>username</u> must contain between " . MIN_USERNAME_LENGTH . "-" . MAX_USERNAME_LENGTH . " character(s).";
+				break;
+			case 3:
+				$_SESSION['error_message'] = "The two <u>passwords</u> you provided do not match.";
+				break;
+			case 4:
+				$_SESSION['error_message'] = "Your <u>password</u> must contain between " . MIN_PASSWORD_LENGTH . "-" . MAX_PASSWORD_LENGTH . " character(s).";
+				break;
+			default:
+				$_SESSION['error_message'] = "This error is reserved for future mist- errrrrrr um, dank-memes.";
+				break;
+		}
+		
+		$_SESSION['error_message'] .= " Please try again.";
+	}
+	
+	else $_SESSION['error_message'] = "<h3 style=\"color:rgb(0,180,0);\">Success!</h3><h4 style=\"color:rgb(0,180,0);\"> Your account has been (hypothetically) created.<br>Hypothetically, you will be able to click <a href=\"#\">here</a> to login.</h4>";
+		
+	echo $html_begin;
+	echo $form_signup;
+	
+	if (isset($_SESSION['error_message'])){
+		
+		echo $html_error_begin;
+		echo $_SESSION['error_message'];
+		echo $html_error_end;
+	}
+	
+	echo $html_end;
+	
+	exit(1);	
+}
+
+else{
+	
+	echo $html_begin;
+	echo $form_signup;
+	echo $html_end;
+}
 
 
-
-
-
-
-echo $html_begin;
-echo $form_signup;
-echo $html_end;
 
 ?>
