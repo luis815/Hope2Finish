f15eed7901bbb8d685d4cfa643882d57fb097a84
vsamaga <43286434+vsamaga@users.noreply.github.com>
2018-10-04 16:51:59 -0400

    Created new upload page
    Users can upload videos here

---
+++ front_end/src/views/Upload.vue
@@ -0,0 +1,34 @@
+<template>
+    <title>Kyros Upload</title>
+    <h1>
+        Kyros
+    </h1>
+    <form action="" method='post' enctype="multipart/form-data">
+        <input type="file" name="file"/><br><br>
+        <input type="text" placeholder="Title"><br><br>
+        <textarea input type="text" placeholder="Description" rows="4" cols="50"></textarea><br><br>
+        <input type="submit" value="Upload"/>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "Upload"
+    }
+</script>
+
+<style scoped>
+    body {
+        text-align: center;
+    }
+    p {
+        font-size: 20px;
+    }
+    .topnav input[type=text] {
+        padding: 6px;
+        border: solid;
+        margin-bottom: 16px;
+        font-size: 17px;
+        width: 35%;
+    }
+</style>
