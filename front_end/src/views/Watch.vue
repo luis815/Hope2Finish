c4b28dcb07272add92258d817bc807833f811f2f
Luis Lopez Cardona <lalopezc@protonmail.com>
2018-10-09 16:34:25 -0400

    Created watch page #22
    1. Created watch vue file.
    2. Added navbar to watch page.
    3. Setup Video.JS
    4. Added video.js video example.
    5. Player plays a placeholder video.

--- front_end/src/views/Watch.vue
+++ front_end/src/views/Watch.vue
@@ -1,17 +1,24 @@
 <template>
     <div id="watch" class="bg-dark">
+        <kyros-bar/>
         <div id="player-container">
             <div class="container">
-                <video id="player" class="video-js" controls preload="auto" height="240px">
-                    <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"/>
-                    <p class="vjs-no-js">
-                        To view this video please enable JavaScript, and consider upgrading to a
-                        web browser that
-                        <a href="http://videojs.com/html5-video-support/" target="_blank">
-                            supports HTML5 video
-                        </a>
-                    </p>
-                </video>
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-10">
+                        <video id="player" class="video-js" autoplay controls preload="auto" style="min-height: 20em">
+                            <source src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"/>
+                            <p class="vjs-no-js">
+                                To view this video please enable JavaScript, and consider upgrading to a
+                                web browser that
+                                <a href="http://videojs.com/html5-video-support/" target="_blank">
+                                    supports HTML5 video
+                                </a>
+                            </p>
+                        </video>
+                    </div>
+                    <div class="col"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -19,9 +26,11 @@
 
 <script>
     import videojs from 'video.js'
+    import KyrosBar from '../components/KyrosBar.vue'
 
     export default {
         name: "Watch",
+        components: {KyrosBar},
         mounted() {
             let options = {fluid: true};
             let player = videojs('player', options);
@@ -38,6 +47,7 @@
         height: 100%;
         overflow: scroll;
     }
+
     #player-container {
         background: black;
     }
