dd443d49dc27e541504c808044f4d2e93b9cb317
Michael Belker <mbelker@teamofweaponsandtactics.com>
2018-11-27 02:00:49 -0500

    upload form and other fecal matter

--- upload.php
+++ upload.php
@@ -82,3 +82,140 @@ define(__NAMESPACE__ . "\DESC_VIDEO_TITLE", "Provide a friendly title for your v
 define(__NAMESPACE__ . "\DESC_VIDEO_DESCRIPTION", "Optionally, provide a description for your video. No tags or HTML please, and no more than " . MAX_VIDEO_DESCRIPTION_LENGTH . " characters.");
 define(__NAMESPACE__ . "\DESC_VIDEO_FILE", "Select the video file you wish to upload. Supported file extensions are: " . $allowed_extension_list . ". Maximum file size is " . \SYSTEM\GET_FRIENDLY_SIZE(MAX_VIDEO_SIZE) . ".");
 define(__NAMESPACE__ . "\DESC_VIDEO_TERMS", "I hereby affirm that by uploading this video, it becomes the sole and exclusive property of Hope2Finish Inc., which shall have full right, title and interest thereto, including under copyright, in all videos now existing or hereafter created, and without any obligation to account or make any payment to the uploader for any use thereof. No purported reservation of rights incorporated in or accompanying any video shall have any force or effect. By checking this box and submitting this video for upload, you hereby agree to all of the foregoing.");
+
+
+
+//	VARS
+//	-------------------------------------------------------------------------------------
+//	-------------------------------------------------------------------------------------
+//	-------------------------------------------------------------------------------------
+//	-------------------------------------------------------------------------------------
+
+$external_css = '<link rel="stylesheet" href="https://cdn.dbmxpca.com/fonts/stylesheet.css" type="text/css" charset="utf-8" />';
+
+$css = 
+'
+
+body {
+	
+	font-family: \'Helvetica Rounded LT Std\';
+    font-weight: bold;
+    font-style: normal;	
+}
+
+label {
+	
+	display: inline-block;
+	float: left;
+	clear: left;
+	width: 150px;
+	text-align: right;
+	margin-right: 10px;
+}
+
+input {
+	
+	display: block;
+	margin-bottom: 5px;
+}
+
+desc {
+	
+	font-size: 14px;
+	color: rgb(150, 150, 150);
+	float: left;
+	margin-left: 175px;
+	margin-top: 0px;
+	/*width: 150px;*/
+	/* display: block; */
+	display: block;
+	/* margin-bottom: 10px; */
+}
+
+
+button {
+	
+	display: inline-block;
+	float: right;
+	font-family: \'Helvetica Rounded LT Std\';
+    font-weight: bold;
+    font-style: normal;
+}
+
+#error {
+	margin: 0 auto;
+	margin-top: 10px;
+	width: 60%;
+	min-width: 200px;
+	color: rgb(255, 0, 0);
+}
+
+
+';
+
+$html_begin = '<html><head><title>Upload A Video | FALL_2018_CSE442_CHRISTOPHER_PEI</title>' . $external_css . '<style> ' . $css . '</style></head><body>';
+$html_end = '</body></html>';
+$html_error_begin = '<div id="error">';
+$html_error_end = '</div>';
+
+$form_upload = '';
+
+if (VIDEO_UPLOADS_ENABLED){
+
+	$form_upload = 
+	'
+	<form action="/upload.php" method="post" enctype="multipart/form-data" style="margin: 0 auto;width:60%;min-width:200px;">
+		<fieldset>
+		
+			<legend><h1>Upload A Video</h1></legend>
+			
+			<label>Title:</label>
+			<input type="text" placeholder="" name="t" size="40" />
+			<desc>' . DESC_VIDEO_TITLE . '</desc>
+			<br><br><br><br>
+			
+			<label>Description:</label>
+			<textarea name="d" rows="4" cols="40"></textarea>
+			
+			<desc>' . DESC_VIDEO_DESCRIPTION . '</desc>
+			<br><br><br><br>
+			
+			<label>Video:</label>
+			<input type="file" name="v" id="v" accept="video/*">
+			<desc>' . DESC_VIDEO_FILE . '</desc>
+			<br><br>
+			
+			<label>Terms and Conditions:</label>
+			<input type="checkbox" value="1" required name="agree">
+			<desc>' . DESC_VIDEO_TERMS . '</desc>
+			
+			<br><br><br><br>
+			<p>&nbsp;</p>
+			
+			<button type="submit">Upload Video</button>
+			
+		</fieldset>
+	</form>
+	';
+}
+
+else{
+	
+	$form_upload = 
+	'
+	<form action="/upload.php" method="post" enctype="multipart/form-data" style="margin: 0 auto;width:60%;min-width:200px;">
+		<fieldset>
+		
+			<legend><h1>Upload A Video</h1></legend>
+			
+			<h2>SORRY</h2>
+			<h3>' . ERR_SERVICE_DISABLED . '</h3>
+			
+		</fieldset>
+	</form>
+	';
+}
+
+
+
+
